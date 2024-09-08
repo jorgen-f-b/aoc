367d3fe880fc7746cf2c1bb1cfe41b6d58790484
jorgen-f-b <jorgen.blomkvist@gmail.com>
2024-09-08 17:02:46 +0200

    aoc-2023 day-1 part-2

--- aoc-2023/day1/main.go
+++ aoc-2023/day1/main.go
@@ -6,8 +6,52 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
+
+var numbers = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+var letterNums = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
+
+func toLetterNum(letterNum string) (string, bool) {
+	var numb, ok = letterNums[letterNum]
+	return numb, ok
+}
+
+func inLetterNum(letterNum string) bool {
+	for _, num := range numbers {
+		if strings.Contains(num, letterNum) {
+			return true
+		}
+	}
+	return false
+}
+
+func removeRunes(letterNum string, reversed bool) string {
+	for i := range letterNum {
+		var newString string
+		if reversed {
+			newString = letterNum[:len(letterNum)-i]
+		} else {
+			newString = letterNum[i:]
+		}
+		if inLetterNum(newString) {
+			return newString
+		}
+	}
+	return ""
+}
 
 func main() {
 	var file, err = os.Open("input1.txt")
@@ -16,16 +60,29 @@ func main() {
 	}
 
 	var scanner = bufio.NewScanner(file)
-	var number int = 0
+	var number = 0
 
 	for scanner.Scan() {
 		var line = scanner.Text()
 		var num = ""
+		var letterNum = ""
 
 		for _, char := range line {
 			if unicode.IsDigit(char) {
 				num += string(char)
+				letterNum = ""
 				break
+			}
+			letterNum += string(char)
+			if inLetterNum(letterNum) {
+				var numb, ok = toLetterNum(letterNum)
+				if ok {
+					num += numb
+					letterNum = ""
+					break
+				}
+			} else {
+				letterNum = removeRunes(letterNum, false)
 			}
 		}
 
@@ -36,7 +93,19 @@ func main() {
 			var r = runeLine[length-i]
 			if unicode.IsDigit(r) {
 				num += string(r)
+				letterNum = ""
 				break
+			}
+			letterNum = string(r) + letterNum
+			if inLetterNum(letterNum) {
+				var numb, ok = toLetterNum(letterNum)
+				if ok {
+					num += numb
+					letterNum = ""
+					break
+				}
+			} else {
+				letterNum = removeRunes(letterNum, true)
 			}
 		}
 
