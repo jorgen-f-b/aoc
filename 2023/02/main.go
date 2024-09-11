f97688efa6e3a43ec692ff2a0a322e73107db136
jorgen-f-b <jorgen.blomkvist@gmail.com>
2024-09-11 20:43:19 +0200

    2023-02-2

--- 2023/02/main.go
+++ 2023/02/main.go
@@ -13,50 +13,7 @@ func trim(s string) string {
 	return strings.Trim(s, " ")
 }
 
-func possibleGames(file *os.File) {
-	bag := map[string]int{
-		"red":   12,
-		"green": 13,
-		"blue":  14,
-	}
-
-	sumPossible := 0
-
-	reader := bufio.NewScanner(file)
-
-	for reader.Scan() {
-		line := reader.Text()
-		res := strings.Split(line, ":")
-		possible := true
-
-		rounds := strings.Split(res[1], ";")
-		for _, round := range rounds {
-			for _, cube := range strings.Split(round, ",") {
-				color := strings.Split(trim(cube), " ")
-				maxAmount, ok := bag[color[1]]
-				if ok {
-					amount, _ := strconv.Atoi(color[0])
-					if amount > maxAmount {
-						possible = false
-						break
-					}
-				} else {
-					possible = false
-					break
-				}
-			}
-			if !possible {
-				break
-			}
-		}
-		if possible {
-			game := strings.Split(res[0], " ")
-			gameNum, _ := strconv.Atoi(game[1])
-			sumPossible += gameNum
-		}
-	}
-	fmt.Println(sumPossible)
-}
+type Bag = map[string]int
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -64,5 +21,57 @@ func main() {
 		log.Fatal(err)
 	}
 	defer file.Close()
-	possibleGames(file)
+
+	bag := Bag{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+
+	sumPossible := 0
+	sumMinimum := 0
+
+	reader := bufio.NewScanner(file)
+
+	for reader.Scan() {
+		line := reader.Text()
+		res := strings.Split(line, ":")
+		rounds := strings.Split(res[1], ";")
+
+		possible := true
+		minAmountBag := make(Bag)
+
+		for _, round := range rounds {
+			for _, cube := range strings.Split(round, ",") {
+				c := strings.Split(trim(cube), " ")
+				color := c[1]
+				amount, _ := strconv.Atoi(c[0])
+
+				bagAmount, ok := minAmountBag[color]
+				if (ok && bagAmount < amount) || !ok {
+					minAmountBag[color] = amount
+				}
+
+				maxAmount, ok := bag[color]
+				if possible && ok && amount > maxAmount {
+					possible = false
+				}
+			}
+		}
+
+		power := 1
+		for _, val := range minAmountBag {
+			power *= val
+		}
+		sumMinimum += power
+
+		if possible {
+			game := strings.Split(res[0], " ")
+			gameNum, _ := strconv.Atoi(game[1])
+			sumPossible += gameNum
+		}
+	}
+
+	fmt.Println(sumPossible)
+	fmt.Println(sumMinimum)
 }
