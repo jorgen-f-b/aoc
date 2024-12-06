43e0b2c0955fd87bbf8d2d3213f0766c9e613529
Jørgen Finjord Blomkvist <joergen.finjord.blomkvist@sparebank1.no>
2024-12-06 13:18:48 +0100

    2024-04-1_2

--- 2024/04/main.go
+++ 2024/04/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
 	"strings"
 )
 
@@ -15,64 +14,99 @@ func handleError(err error) {
 	}
 }
 
+func correctWord(s string) bool {
+	return s == "MAS" || s == "SAM"
+}
+
+func searchForX(arr []string) bool {
+	word1 := string(arr[0][0]) + string(arr[1][1]) + string(arr[2][2])
+	word2 := string(arr[0][2]) + string(arr[1][1]) + string(arr[2][0])
+	return correctWord(word1) && correctWord(word2)
+}
+
+func searchForWord(arr []string, row int, col int, searchWord string) int {
+	liggende := ""
+	lf := 0
+	staende := ""
+	sf := 0
+	skraH := ""
+	shf := 0
+	skraV := ""
+	svf := 0
+
+	line := arr[row]
+	for i := col; i < len(line); i++ {
+		liggende += string(line[i])
+		if !strings.Contains(searchWord, liggende) {
+			break
+		}
+		if searchWord == liggende {
+			lf++
+			break
+		}
+	}
+
+	j := 0
+	for i := row; i < len(arr) && i < row+len(searchWord); i++ {
+		staende += string(arr[i][col])
+		if col+j < len(arr[i]) {
+			skraH += string(arr[i][col+j])
+		}
+		if col-j >= 0 {
+			skraV += string(arr[i][col-j])
+		}
+
+		if !strings.Contains(searchWord, staende) && !strings.Contains(searchWord, skraH) && !strings.Contains(searchWord, skraV) {
+			break
+		}
+
+		if sf == 0 && searchWord == staende {
+			sf++
+		}
+		if shf == 0 && searchWord == skraH {
+			shf++
+		}
+		if svf == 0 && searchWord == skraV {
+			svf++
+		}
+		j++
+	}
+
+	return lf + sf + shf + svf
+}
+
 func main() {
-	file, err := os.Open("example.txt")
+	file, err := os.Open("input.txt")
 	handleError(err)
+	defer file.Close()
 
-	reg, err := regexp.Compile(`XMAS|SAMX`)
-	handleError(err)
-	reg2, err := regexp.Compile(`X.{10}M.{10}A.{10}S|S.{10}A.{10}M.{10}X`)
-	handleError(err)
-
-	sideway := []string{}
+	strArr := []string{}
+	sum := 0
+	sum2 := 0
 
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
 		text := scanner.Text()
-		sideway = append(sideway, text)
+		strArr = append(strArr, text)
 	}
 
-	matches1 := reg.FindAllStringIndex(strings.Join(sideway, ","), -1)
-	matches2 := reg2.FindAllStringIndex(strings.Join(sideway, ","), -1)
+	for i, linje := range strArr {
+		for j, r := range linje {
+			if r == 'X' {
+				sum += searchForWord(strArr, i, j, "XMAS")
+			}
+			if r == 'S' {
+				sum += searchForWord(strArr, i, j, "SAMX")
+			}
+			if (r == 'M' || r == 'S') && i+2 < len(strArr) && j+2 < len(linje) {
+				if searchForX([]string{strArr[i][j : j+3], strArr[i+1][j : j+3], strArr[i+2][j : j+3]}) {
+					sum2++
+				}
+			}
+		}
+	}
 
-	// standing := []string{}
-	// crossRight := []string{}
-
-	// for i, linje := range sideway {
-	// 	matches := reg.FindAllStringIndex(linje, -1)
-	// 	count += len(matches)
-
-	// 	for j, r := range linje {
-	// 		if i == 0 {
-	// 			standing = append(standing, string(r))
-	// 			crossRight = append(crossRight, string(r))
-	// 			continue
-	// 		}
-
-	// 		standing[j] = standing[j] + string(r)
-	// 		if j == 0 {
-	// 			crossRight = append(crossRight, string(r))
-	// 		} else {
-	// 			if j >= i {
-	// 				crossRight[j-i] = crossRight[j-i] + string(r)
-	// 			} else {
-	// 				crossRight[i-j] = crossRight[i-j] + string(r)
-	// 			}
-	// 		}
-	// 	}
-	// }
-
-	// for _, linje := range standing {
-	// 	matches := reg.FindAllStringIndex(linje, -1)
-	// 	count += len(matches)
-	// }
-	// for _, linje := range crossRight {
-	// 	matches := reg.FindAllStringIndex(linje, -1)
-	// 	count += len(matches)
-	// }
-
-	// fmt.Println(sideway)
-	// fmt.Println(crossRight)
-	fmt.Println("Count", len(matches1)+len(matches2))
+	fmt.Println("Sum", sum)
+	fmt.Println("Sum2", sum2)
 }
