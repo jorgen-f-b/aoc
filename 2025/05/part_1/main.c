e48e82a8853e186d4b41ee1b6c0b26cd65ee3b7f
jorgen-f-b <jorgen.blomkvist@gmail.com>
2025-12-10 19:35:08 +0100

    2025-05-1

--- 2025/05/part_1/main.c
+++ 2025/05/part_1/main.c
@@ -2,7 +2,7 @@
 #include "../../base.h"
 #include "../../read_file.c"
 
-#define FILE_SIZE 512
+#define FILE_SIZE 21373
 #define STR_NUM_LEN 20
 
 typedef struct {
@@ -17,7 +17,7 @@ typedef struct {
     bool get_range;
     union{
         Range range;
-        int id;
+        u64 id;
    };
 } Iter_Input;
 #define iter_input_lit(I, S) { I, S, 0, true }
@@ -27,7 +27,10 @@ u64 str_to_u64(char *str) {
 }
 
 bool get_ranges(Iter_Input *iter) {
-    if (iter->input[iter->idx] == '\n') return false;
+    if (iter->input[iter->idx] == '\n') {
+        iter->idx++;
+        return false;
+    }
 
     char from[STR_NUM_LEN] = {0};
     char to[STR_NUM_LEN] = {0};
@@ -51,12 +54,41 @@ bool get_ranges(Iter_Input *iter) {
     return true;
 }
 
+bool get_ids(Iter_Input *iter) {
+    if (iter->idx >= iter->input_size) return false;
+
+    char id[STR_NUM_LEN] = {0};
+    int idx = 0;
+    while (iter->input[iter->idx] != '\n') id[idx++] = iter->input[iter->idx++];
+    iter->idx++;
+    iter->id = str_to_u64(id);
+
+    return true;
+}
+
+bool is_fresh(u64 id, Range *ranges, int range_size) {
+    for (int i = 0; i < range_size; i++) {
+        if (id >= ranges[i].from && id <= ranges[i].to) return true;
+    }
+    return false;
+}
+
 int main() {
     char file[FILE_SIZE];
-    int file_size = read_entire_file("../example.txt", file, FILE_SIZE);
+    int file_size = read_entire_file("../input.txt", file, FILE_SIZE);
+    //int file_size = read_entire_file("../example.txt", file, FILE_SIZE);
 
+    Range ranges[200];
+    int range_size = 0;
     Iter_Input iter = iter_input_lit(file, file_size);
-    while (get_ranges(&iter)) printf("Range{ %lld, %lld }\n", iter.range.from, iter.range.to);
+    while (get_ranges(&iter)) ranges[range_size++] = iter.range;
+
+    int res = 0;
+    while (get_ids(&iter)) {
+        if (is_fresh(iter.id, ranges, range_size)) res++;
+    }
+
+    printf("res: %d\n", res);
 
     return 0;
 }
